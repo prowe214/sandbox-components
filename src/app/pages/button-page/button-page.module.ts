c437147f7606ac53750755e5473055fb879f3c77
Paul Rowe <paul.rowe@crownpeak.com>
2017-01-19 11:18:50 -0700

    updated

--- src/app/pages/button-page/button-page.module.ts
+++ src/app/pages/button-page/button-page.module.ts
@@ -1,15 +1,12 @@
-import { ButtonPageRoutingModule } from './button-page-routing.module';
 import { CplButtonComponent } from './../../../../cp-component-library/lib/components/cpl-button/cpl-button.component';
 import { CplButtonModule } from './../../../../cp-component-library/lib/components/cpl-button/cpl-button.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonPageComponent } from './button-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CplButtonModule,
-    ButtonPageRoutingModule
   ],
   exports: [
     CplButtonComponent
